737fc29dfc85f43dd9a710ac8e069c14dfcd2424
py-ovarion <olegk6783@gmail.com>
2026-02-04 22:00:47 +0200

    multiple exception parenthesized

--- app/main.py
+++ app/main.py
@@ -83,7 +83,7 @@ class Slide:
     def can_access(self, visitor: Visitor) -> bool:
         try:
             self.limitation_class(visitor.age, visitor.weight, visitor.height)
-        except ValueError, TypeError:
+        except (ValueError, TypeError):
             return False
         return True
 
